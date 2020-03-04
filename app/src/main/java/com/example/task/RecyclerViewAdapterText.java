a1bcf444375d58b1273c67b67b2776f37e840cf0
siddharth <siddharth.kumar@bobble.ai>
2020-03-04 19:44:55 +0530

    monday_finish

--- app/src/main/java/com/example/task/RecyclerViewAdapterText.java
+++ app/src/main/java/com/example/task/RecyclerViewAdapterText.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -26,14 +29,57 @@ public class RecyclerViewAdapterText extends RecyclerView.Adapter<RecyclerViewAd
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_layout,parent,false);
 
 
+
+
         return new ViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
 
         holder.tv1.setText(lang.get(position).t1);
         holder.tv2.setText(lang.get(position).t2);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Toast.makeText(mcontext, holder.tv1.getText(), Toast.LENGTH_SHORT).show();
+                final AlertDialog.Builder alert = new AlertDialog.Builder(mcontext);
+                LayoutInflater inflater = (LayoutInflater) mcontext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+               // View v2 = inflater.inflate(R.layout.dialog ,null);
+                View mview = inflater.inflate(R.layout.dialog,null);
+                Button btn_cancel=(Button)mview.findViewById(R.id.btn_cancel);
+                TextView t1=mview.findViewById(R.id.hinditext);
+                t1.setText(holder.tv1.getText());
+
+
+
+                alert.setView(mview);
+                final AlertDialog alertDialog = alert.create();
+                alertDialog.setCanceledOnTouchOutside(true);
+
+               btn_cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        alertDialog.dismiss();
+                    }
+                });
+
+
+                Button btn_done=(Button)mview.findViewById(R.id.btn_done);
+
+                btn_done.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        alertDialog.dismiss();
+                    }
+                });
+
+                alertDialog.show();
+            }
+        });
 
 
     }
